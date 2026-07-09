e6809f3fd8f7beadfcc32890cc3ad3d72e315b4e
jjavierrios-del <jjavierrios@gmail.com>
2026-07-09 06:28:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cuadrante-pwa-v19";
+const CACHE = "cuadrante-pwa-v20";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-180.png"];
 
 self.addEventListener("install", (e) => {
